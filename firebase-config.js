adfe1b28cc132b666bafd10fcbf27a8b2eb18e40
javiernovillo2-byte <javiernovillo2@gmail.com>
2026-05-11 10:27:05 -0300

    Add Firebase configuration and initialization

---
+++ firebase-config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDwUirH-Ylk5WkTPCpPAMuU5l4bWLDljVo",
+  authDomain: "pizzeria-pro-e0964.firebaseapp.com",
+  databaseURL: "https://pizzeria-pro-e0964-default-rtdb.firebaseio.com",
+  projectId: "pizzeria-pro-e0964",
+  storageBucket: "pizzeria-pro-e0964.firebasestorage.app",
+  messagingSenderId: "820646614801",
+  appId: "1:820646614801:web:e03f1a15e696d95bbe5eb0"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
